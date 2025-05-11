a92a26e63afd019b87685b2bf18106ff03b00a12
Roman1019 <142348028+Roman1019@users.noreply.github.com>
2025-05-11 13:12:29 +0300

    second commit

--- src/contacts.js
+++ src/contacts.js
@@ -1,16 +1,9 @@
 import { Contact } from '../models/contactsSchema.js';
 
 export async function getAllContactsService() {
-  return await Contact.find(); // Повертає всі контакти
+  return await Contact.find();
 }
-// import app from './server.js';
-// import express from 'express';
-// import mongoose from 'mongoose';
-// import { Contact } from './models/contactsSchema.js';
 
-// app.get('/api/contacts', async (req, res) => {
-//   const contacts = await Contact.find();
-//   res.json({
-//     data: contacts,
-//   });
-// });
+export async function getContactById(id) {
+  return await Contact.findById(id);
+}
--- src/server.js
+++ src/server.js
@@ -2,8 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
 import 'dotenv/config';
-
 import { Contact } from './models/contactsSchema.js';
+import mongoose from 'mongoose';
 
 function setupServer() {
   const app = express();
@@ -13,16 +13,32 @@ function setupServer() {
 
   app.get('/api/contacts', async (req, res) => {
     try {
-      const contacts = await Contact.find(); // отримуємо всі контакти
+      const contacts = await Contact.find();
       res.json({
         data: contacts,
       });
     } catch (error) {
-      console.error('Error fetching contacts:', error); // Логування помилки
+      console.error('Error fetching contacts:', error);
       res
         .status(500)
         .json({ message: 'Error retrieving contacts', error: error.message });
     }
+  });
+
+  app.get('/api/contacts/:id', async (req, res) => {
+    const id = req.params.id;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ message: 'Invalid contact Id' });
+    }
+
+    const contact = await Contact.findById(id);
+    console.log(contact);
+    if (contact === null) {
+      return res.status(404).send({ message: 'Contact not found' });
+    }
+    res.json({
+      data: contact,
+    });
   });
 
   app.use((req, res) => {
