050089d25f2e0f965c7c6b24fdf6704b84537db0
Roman1019 <142348028+Roman1019@users.noreply.github.com>
2025-05-27 12:19:31 +0300

    correct isValidId commit

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -2,7 +2,7 @@ import createHttpError from 'http-errors';
 import { isValidObjectId } from 'mongoose';
 
 export function isValidId(req, res, next) {
-  if (isValidObjectId(req.params.id) !== true) {
+  if (isValidObjectId(req.params.contactId) !== true) {
     return next(createHttpError.BadRequest('Id should be an ObjectId'));
   }
 
